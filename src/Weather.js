a6d662624095895e787c542b1a861b061f371e55
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2021-02-09 20:54:51 +0100

    Loader added

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios';
+import Loader from "react-loader-spinner";
 
 
 export default function Weather(props) {
@@ -9,7 +10,13 @@ export default function Weather(props) {
     let apiKey = "34f1c513b53f57aa71f2dd7697c02049";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return (<h2>Hello from Weather</h2>)
+    return ( <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />)
     
    
 }
