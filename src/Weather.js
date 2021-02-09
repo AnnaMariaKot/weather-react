f11625ffe4cf410f27a5fd7e7695cb1e789bac85
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2021-02-09 20:58:08 +0100

    Changed color loader

--- src/Weather.js
+++ src/Weather.js
@@ -12,10 +12,10 @@ export default function Weather(props) {
     axios.get(apiUrl).then(handleResponse);
     return ( <Loader
         type="Puff"
-        color="#00BFFF"
+        color="orange"
         height={100}
         width={100}
-        timeout={3000} //3 secs
+        timeout={3000} 
       />)
     
    
